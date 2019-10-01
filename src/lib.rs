f60145108eb85cf44ae00b34670f11607e0718ed
Brooks Patton <brooks@brookzerker.com>
2019-10-01 07:37:59 -0600

    Created feature, tests are sometimes failing for the random bright color which is unrelated to this issue

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,10 @@ impl Utility {
 	pub fn random_bright_color(&mut self) -> Color {
 		Color::from_rgb(self.rng.gen_range(175, 255), self.rng.gen_range(175, 255), self.rng.gen_range(175, 255))
 	}
+
+	pub fn random_dark_color(&mut self) -> Color {
+		Color::new(self.rng.gen_range(0.0, 0.51), self.rng.gen_range(0.0, 0.51), self.rng.gen_range(0.0, 0.51), 1.0)
+	}
 }
 
 #[cfg(test)]
@@ -36,4 +40,22 @@ mod tests {
 		assert!(color.b >= 0.7 && color.b <= 1.0);
 		assert_eq!(color.a, 1.0);
     }
+
+	#[test]
+	fn creates_a_random_dark_color() {
+		let mut utility = Utility::new();
+		let color = utility.random_dark_color();
+		let another_color = utility.random_dark_color();
+
+		assert!(color.r < 0.5 && color.r >= 0.0);
+		assert!(color.g < 0.5 && color.g >= 0.0);
+		assert!(color.b < 0.5 && color.b >= 0.0);
+		assert_eq!(color.a, 1.0);
+
+		assert!(
+			color.r != another_color.r &&
+			color.g != another_color.g &&
+			color.b != another_color.b
+		);
+	}
 }
