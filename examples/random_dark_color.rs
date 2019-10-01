082620f7d6c497d5fee7c02f08dfa76708e091b1
Brooks Patton <brooks@brookzerker.com>
2019-10-01 07:42:34 -0600

    created example

---
+++ examples/random_dark_color.rs
@@ -0,0 +1,61 @@
+extern crate bbggez;
+
+use ggez::{
+	ContextBuilder, graphics, event, Context, GameResult, timer,
+	event::EventHandler,
+	nalgebra::Point2,
+	graphics::{Color, MeshBuilder, DrawMode}
+};
+use bbggez::Utility;
+
+struct Game {
+	utility: Utility,
+	color: Color
+}
+
+impl Game {
+	pub fn new(_context: &mut Context) -> Game {
+		let mut utility = Utility::new();
+
+		Game {
+			utility,
+			color: utility.random_dark_color()
+		}
+	}
+}
+
+impl EventHandler for Game {
+	fn update(&mut self, context: &mut Context) -> GameResult<()> {
+		if timer::ticks(context) % 1000 == 0 {
+			self.color = self.utility.random_dark_color();
+		}
+
+		Ok(())
+	}
+
+	fn draw(&mut self, context: &mut Context) -> GameResult<()> {
+		graphics::clear(context, graphics::WHITE);
+
+		let (width, height) = graphics::drawable_size(context);
+
+		let circle = MeshBuilder::new()
+			.circle(DrawMode::fill(), Point2::new(width / 2.0, height / 2.0), 200.0, 0.1, self.color)
+			.build(context)?;
+
+		graphics::draw(context, &circle, (Point2::new(0.0, 0.0),))?;
+
+		graphics::present(context)
+	}
+}
+
+fn main() {
+	let (mut context, mut event_loop) = ContextBuilder::new("Random color example", "Brookzerker")
+		.build()
+		.expect("Game context was not able to be created");
+	let mut game = Game::new(&mut context);
+
+	match event::run(&mut context, &mut event_loop, &mut game) {
+		Ok(_) => println!("Exited cleanly"),
+		Err(error) => println!("Error occured: {}", error),
+	};
+}
