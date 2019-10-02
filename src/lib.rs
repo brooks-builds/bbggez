a9ba5f3849f1af8e4609a51fbeefd475d94f7842
Andreu Cotanda Oliver <cotoli23@gmail.com>
2019-10-02 19:11:45 +0200

    Added create_rectangle and create_square function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use ggez::{
-	graphics::{Color, DrawMode, Mesh, MeshBuilder},
+	graphics::{Color, DrawMode, Mesh, MeshBuilder, Rect},
 	nalgebra::Point2,
 	Context,
 };
@@ -48,6 +48,37 @@ impl Utility {
 	) -> Mesh {
 		MeshBuilder::new()
 			.circle(DrawMode::fill(), Point2::new(x, y), radius, 0.01, color)
+			.build(ctx)
+			.unwrap()
+	}
+
+	pub fn create_rect(
+		&mut self,
+		x: f32,
+		y: f32,
+		width: f32, 
+		height: f32,
+		color: Color,
+		ctx: &mut Context,
+	) -> Mesh {
+		let rect = Rect::new(x, y, width, height);
+		MeshBuilder::new()
+			.rectangle(DrawMode::fill(), rect, color)
+			.build(ctx)
+			.unwrap()
+	}
+
+	pub fn create_square(
+		&mut self,
+		x: f32,
+		y: f32,
+		side: f32, 
+		color: Color,
+		ctx: &mut Context,
+	) -> Mesh {
+		let rect = Rect::new(x, y, side, side);
+		MeshBuilder::new()
+			.rectangle(DrawMode::fill(), rect, color)
 			.build(ctx)
 			.unwrap()
 	}
