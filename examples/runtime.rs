55b8244d9a5372e9a5108544eab7aa4c26218a20
Danny Fritz <dannyfritz@gmail.com>
2019-10-04 20:47:42 -0600

    rust fmt

--- examples/runtime.rs
+++ examples/runtime.rs
@@ -1,33 +1,28 @@
 extern crate bbggez;
-use bbggez::ggez::{
-  event::EventHandler,
-  graphics,
-  Context,
-  GameResult,
-};
+use bbggez::ggez::{event::EventHandler, graphics, Context, GameResult};
 
 struct Game {}
 
 impl Game {
-  fn new () -> Game {
-    Game {}
-  }
+    fn new() -> Game {
+        Game {}
+    }
 }
 
 impl EventHandler for Game {
-  fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-    println!("update");
-    return Ok(())
-  }
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        println!("update");
+        return Ok(());
+    }
 
-  fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-    graphics::clear(ctx, graphics::WHITE);
-    println!("draw");
-    graphics::present(ctx)
-  }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+        println!("draw");
+        graphics::present(ctx)
+    }
 }
 
 fn main() {
-  let mut game = Game::new();
-  bbggez::run(&mut game, "Runtime", "Brookzerker")
+    let mut game = Game::new();
+    bbggez::run(&mut game, "Runtime", "Brookzerker")
 }
