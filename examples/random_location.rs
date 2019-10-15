2c06faa63b3fad7698f89bda18f03f2dcfc6e80f
Wai Pai Lee <pailee.wai@gmail.com>
2019-10-15 10:54:47 +0800

    Uses utility build-in function

--- examples/random_location.rs
+++ examples/random_location.rs
@@ -1,11 +1,10 @@
 extern crate bbggez;
 
-use bbggez::{rand, Utility};
+use bbggez::Utility;
 use ggez::{
-    event,
     event::EventHandler,
     graphics,
-    graphics::{Color, DrawMode, MeshBuilder},
+    graphics::Color,
     nalgebra::Point2,
     timer::{delta, duration_to_f64},
     Context, ContextBuilder, GameResult,
@@ -36,15 +35,8 @@ impl EventHandler for Game {
         let (width, height) = graphics::drawable_size(context);
         self.timer = self.timer - duration_to_f64(delta(context));
         if self.timer <= 0.0 {
-            let mut location = self.utility.random_location(width / 2.0, height / 2.0);
+            let location = self.utility.random_location(width / 2.0, height / 2.0);
 
-            if rand::random() {
-                location.x = location.x * -1.0;
-            }
-
-            if rand::random() {
-                location.y = location.y * -1.0;
-            }
             self.point = Point2::new(location.x, location.y);
             self.timer = 1.0;
         }
@@ -55,17 +47,9 @@ impl EventHandler for Game {
     fn draw(&mut self, context: &mut Context) -> GameResult<()> {
         graphics::clear(context, graphics::BLACK);
 
-        let (width, height) = graphics::drawable_size(context);
-
-        let circle = MeshBuilder::new()
-            .circle(
-                DrawMode::fill(),
-                Point2::new(width / 2.0, height / 2.0),
-                200.0,
-                0.1,
-                self.color,
-            )
-            .build(context)?;
+        let circle =
+            self.utility
+                .create_circle(self.point.x, self.point.y, 200.0, self.color, context);
 
         graphics::draw(context, &circle, (self.point,))?;
 
@@ -74,14 +58,10 @@ impl EventHandler for Game {
 }
 
 fn main() {
-    let (mut context, mut event_loop) =
+    let (mut context, mut _event_loop) =
         ContextBuilder::new("Random location example", "Brookzerker")
             .build()
             .expect("Game context was not able to be created");
     let mut game = Game::new(&mut context);
-
-    match event::run(&mut context, &mut event_loop, &mut game) {
-        Ok(_) => println!("Exited cleanly"),
-        Err(error) => println!("Error occured: {}", error),
-    };
+    bbggez::run(&mut game, "Random location example", "Brookzerker");
 }
